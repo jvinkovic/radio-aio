840e440fe67d35978e85adb4f700a30d68031a7f
jvinkovic <josip.vinkovic.josip@gmail.com>
2025-08-09 02:22:32 +0200

    fixes

--- src/components/CurrentSong.jsx
+++ src/components/CurrentSong.jsx
@@ -1,69 +1,73 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import useInterval from "use-interval-hook";
-import PropTypes from 'prop-types';
-import genericCover from '../assets/cover.jpg';
+import PropTypes from "prop-types";
+import genericCover from "../assets/cover.jpg";
 import ModalImage from "react-modal-image";
 
-const CurrentSong = ({url, songDataFunc, checkInterval, onTitleClicked, isPlaying = false}) => {
-    const [songData, setSongData] = useState(null);
+const CurrentSong = ({ url, songDataFunc, checkInterval, onTitleClicked, isPlaying = false }) => {
+  const [songData, setSongData] = useState(null);
 
-    const getData = () => {
-      if(!!songDataFunc && !!url) {
-        songDataFunc(url, setSongData);
-      }
+  const getData = () => {
+    if (!!songDataFunc) {
+      songDataFunc(url, setSongData);
     }
+  };
 
-    const {
-        stop,
-      } = useInterval({
-        interval: (checkInterval || 30)*1000,
-        callback: getData,
-        delay: 300
-      });
+  const { stop } = useInterval({
+    interval: (checkInterval || 30) * 1000,
+    callback: getData,
+    delay: 300,
+  });
 
-    useEffect(() => {
-        getData();
+  useEffect(() => {
+    getData();
 
-        return function cleanup() {
-          stop();
-        };
+    return function cleanup() {
+      stop();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [url]);
+  }, [url]);
 
-    let title = songData?.nowplaying;
-    if(!title) {
-      title = "??";
-    }
+  let title = songData?.nowplaying;
+  if (!title) {
+    title = "??";
+  }
 
-    const cover = songData?.coverart;
-    const artist = songData?.artist;
+  const cover = songData?.coverart;
+  const artist = songData?.artist;
 
-    return (<>
-            <h3 className='song-title' style={{cursor: !!onTitleClicked ? 'pointer' : 'auto'}} 
-              onClick={onTitleClicked}>{title}</h3>
-            <h4 className='artist'>{artist}</h4>
-            {isPlaying && 
-              <ModalImage
-                className='cover-img'
-                small={cover || genericCover}
-                medium={cover || genericCover}
-                hideDownload={true}
-                hideZoom={true}
-                imageBackgroundColor='grey'/>
-            }
-            {!isPlaying && 
-              <img src={cover || genericCover}
-                style={{cursor: !!onTitleClicked ? 'pointer' : 'auto'}} 
-                className='cover-img'
-                onClick={onTitleClicked}/>
-            }
-        </>);
-}
+  return (
+    <>
+      <h3 className="song-title" style={{ cursor: !!onTitleClicked ? "pointer" : "auto" }} onClick={onTitleClicked}>
+        {title}
+      </h3>
+      <h4 className="artist">{artist}</h4>
+      {isPlaying && (
+        <ModalImage
+          className="cover-img"
+          small={cover || genericCover}
+          medium={cover || genericCover}
+          hideDownload={true}
+          hideZoom={true}
+          imageBackgroundColor="grey"
+        />
+      )}
+      {!isPlaying && (
+        <img
+          src={cover || genericCover}
+          style={{ cursor: !!onTitleClicked ? "pointer" : "auto" }}
+          className="cover-img"
+          onClick={onTitleClicked}
+        />
+      )}
+    </>
+  );
+};
 
 CurrentSong.propTypes = {
   url: PropTypes.string,
   songDataFunc: PropTypes.func,
-  checkInterval: PropTypes.number
+  checkInterval: PropTypes.number,
 };
 /*
 songDataFunc = {
--- src/shared/Streams.js
+++ src/shared/Streams.js
@@ -1,171 +1,170 @@
 const getDataSocket = (url, onMessageFunc) => {
-    const socket = new WebSocket(url);
+  const socket = new WebSocket(url);
 
-    socket.onopen = function(event) {
+  socket.onopen = function (event) {
     // Handle connection open
-    };
+  };
 
-    socket.onmessage = function(event) {
-        onMessageFunc(event.data);
-    };
+  socket.onmessage = function (event) {
+    onMessageFunc(event.data);
+  };
 
-    socket.onclose = function(event) {
+  socket.onclose = function (event) {
     // Handle connection close
-    };
+  };
 
-    const sendMessage = (message) => {
-        socket.send(message);
-    }
+  const sendMessage = (message) => {
+    socket.send(message);
+  };
 
-    return {};
-}
-
+  return {};
+};
 
 const shoutemDataRead = (url) => {
-    return fetch('https://api.allorigins.win/get?url=' + encodeURIComponent(url))
-            .then(response => response.json())
-            .then(result => {            
-                const htmlObject = document.createElement('div');
-                htmlObject.innerHTML = result?.contents;
-                const info = htmlObject.innerText;
-    
-                if(!info) {
-                    return;
-                }
-    
-                const listeners = info.split(',')[0];
-                let currentInfo = info.split(',');
-                currentInfo.reverse();
-                currentInfo = currentInfo[0];
-                const artist = currentInfo.split(' - ')[0];
-                const title = currentInfo.split(' - ')[1];
-                const data = {
-                    nowplaying: `${title} [#${listeners}]`,
-                    artist: artist,                        
-                };
-                
-                return data;
-            });
-}
+  return fetch("https://api.allorigins.win/get?url=" + encodeURIComponent(url))
+    .then((response) => response.json())
+    .then((result) => {
+      const htmlObject = document.createElement("div");
+      htmlObject.innerHTML = result?.contents;
+      const info = htmlObject.innerText;
+
+      if (!info) {
+        return;
+      }
+
+      const listeners = info.split(",")[0];
+      let currentInfo = info.split(",");
+      currentInfo.reverse();
+      currentInfo = currentInfo[0];
+      const artist = currentInfo.split(" - ")[0];
+      const title = currentInfo.split(" - ")[1];
+      const data = {
+        nowplaying: `${title} [#${listeners}]`,
+        artist: artist,
+      };
+
+      return data;
+    });
+};
 
 const tamburaskiSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: '',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "",
+    artist: "??",
+  };
 
-    fetch(url)
-        .then(r => r.json())
-        .then(result => {
-            const album = result?.album || ' / ';
-            data.nowplaying = `${result.title} (${album})`;
-            data.coverart = result.cover;
-            data.artist = result.artist;
-        })
-        .catch(error => console.warn('Fetch error:', error))
-        .finally(() => {
-            setFunc(data);
-        });
+  fetch(url)
+    .then((r) => r.json())
+    .then((result) => {
+      const album = result?.album || " / ";
+      data.nowplaying = `${result.title} (${album})`;
+      data.coverart = result.cover;
+      data.artist = result.artist;
+    })
+    .catch((error) => console.warn("Fetch error:", error))
+    .finally(() => {
+      setFunc(data);
+    });
 };
 
 const DRSSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: 'https://amu.me/wp-content/uploads/2018/04/DRS-logo3-300x192-1.jpg',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "https://amu.me/wp-content/uploads/2018/04/DRS-logo3-300x192-1.jpg",
+    artist: "??",
+  };
 
-    shoutemDataRead(url)
-        .then(r => {
-            data.nowplaying = r.nowplaying;
-            data.artist = r.artist;
-        })
-        .catch(error => console.warn('Fetch error:', error))
-        .finally(() => {
-            setFunc(data);
-        });
+  shoutemDataRead(url)
+    .then((r) => {
+      data.nowplaying = r.nowplaying;
+      data.artist = r.artist;
+    })
+    .catch((error) => console.warn("Fetch error:", error))
+    .finally(() => {
+      setFunc(data);
+    });
 };
 
 const fortunaSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: 'https://radiofortuna.com.mk/wp-content/uploads/2024/06/fortuna_herer_logo_544.png',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "https://cdn.onlineradiobox.com/img/l/7/13697.v4.png",
+    artist: "??",
+  };
 
-    setFunc(data);
+  setFunc(data);
 };
 
 const banovinaSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: '',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "",
+    artist: "??",
+  };
 
-    fetch(url)
-        .then(r => r.json())
-        .then(result => {
-            data.nowplaying = result.nowplaying;
-            data.coverart = result.coverart;
-        })
-        .catch(error => console.warn('Fetch error:', error))
-        .finally(() => {
-            setFunc(data);
-        });
+  fetch(url)
+    .then((r) => r.json())
+    .then((result) => {
+      data.nowplaying = result.nowplaying;
+      data.coverart = result.coverart;
+    })
+    .catch((error) => console.warn("Fetch error:", error))
+    .finally(() => {
+      setFunc(data);
+    });
 };
 
 const radioDjakovoSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: 'https://www.radio-djakovo.hr/wp-content/uploads/2018/06/Radio-Djakovo-live-stream-logo-final.jpg',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "https://www.radio-djakovo.hr/wp-content/uploads/2018/06/Radio-Djakovo-live-stream-logo-final.jpg",
+    artist: "??",
+  };
 
-    setFunc(data);
+  setFunc(data);
 };
 
 const slavonskiSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: 'https://slavonskiradio.hr/wp-content/uploads/2024/09/LOGO-SLAVONSKI-COLOR-PNG.png',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "https://slavonskiradio.hr/wp-content/uploads/2024/09/LOGO-SLAVONSKI-COLOR-PNG.png",
+    artist: "??",
+  };
 
-    setFunc(data);
+  setFunc(data);
 };
 
 const antenaSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: 'https://www.antenazagreb.hr/wp-content/uploads/2018/03/ANTENA-LOGOTIP-2018-color-landscape-2.png',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "https://www.antenazagreb.hr/wp-content/uploads/2018/03/ANTENA-LOGOTIP-2018-color-landscape-2.png",
+    artist: "??",
+  };
 
-    fetch('https://api.allorigins.win/get?url=' + encodeURIComponent(url))
-        .then(response => response.json())
-        .then(result => {
-            data.nowplaying = `${result?.contents.split(' - ')[1]}`;
-            data.artist = result?.contents.split(' - ')[0];
-        })
-        .catch(error => console.warn('Fetch error:', error))
-        .finally(() => {
-            setFunc(data);
-        });
+  fetch("https://api.allorigins.win/get?url=" + encodeURIComponent(url))
+    .then((response) => response.json())
+    .then((result) => {
+      data.nowplaying = `${result?.contents.split(" - ")[1]}`;
+      data.artist = result?.contents.split(" - ")[0];
+    })
+    .catch((error) => console.warn("Fetch error:", error))
+    .finally(() => {
+      setFunc(data);
+    });
 };
 
 const otvoreniSongDataFunc = (url, setFunc) => {
-    const data = {
-        nowplaying: '??',
-        coverart: 'https://www.otvoreni.hr/ea/wp-content/themes/otvoreni-wp/assets/art/logo-v3.png',
-        artist: '??'
-    };
+  const data = {
+    nowplaying: "??",
+    coverart: "https://static.mytuner.mobi/media/tvos_radios/wSGYWxvtqZ.jpg",
+    artist: "??",
+  };
 
-    getDataSocket(url, (result) => {
-        data.nowplaying = result.nowplaying;
-        setFunc(data);
-    });
+  getDataSocket(url, (result) => {
+    data.nowplaying = result.nowplaying;
+    setFunc(data);
+  });
 };
 
 /////////////////////
@@ -181,88 +180,106 @@ const otvoreniSongDataFunc = (url, setFunc) => {
     frequencies: FM frequencies on which radio is available (MHz)
 */
 const streams = [
-    { 
-        name: 'Tamburaški',
-        url: 'https://listen.radioking.com/radio/552965/stream/612287',
-        web: 'https://www.tamburaski.com/',        
-        currentSongUrl: 'https://api.radioking.io/widget/radio/tamburaskiradio/track/current',
-        currentSongDataFunc: tamburaskiSongDataFunc,
-    },
-    { 
-        name: 'DRS',
-        url: 'https://eu2.fastcast4u.com/proxy/mic0?mp=/stream',  
-        web: 'https://drugacija.me/',
-        frequencies: ['104,8', '101,5', '97,1', '95,5', '93,0', '90,3'],
-        currentSongUrl: 'https://eu2.fastcast4u.com/proxy/mic0/7.html',
-        currentSongDataFunc: DRSSongDataFunc,
-    },
-    {
-        name: 'Radio Fortuna MK',
-        url: 'https://radiofortuna.ipradio.mk/;',
-        web: 'https://radiofortuna.com.mk/',
-        historyUrl: 'https://radiofortuna.ipradio.mk/played.html',
-        currentSongDataFunc: fortunaSongDataFunc
-    },
-    { 
-        name: 'Banovina Uživo',
-        url: 'https://audio.radio-banovina.hr:9998/stream',
-        web: 'https://www.radio-banovina.hr/',
-        frequencies: ['99,1', '96,8', '93,2'],
-        historyUrl: 'https://www.radio-banovina.hr/uzivo/song_history.html',
-        currentSongUrl: 'https://pool.alter-media.hr:2020/json/stream/banovina',
-        currentSongDataFunc: banovinaSongDataFunc
-    },
-    { 
-        name: 'Banovina Light',
-        url: 'https://audio.radio-banovina.hr:7008/stream',
-        web: 'https://www.radio-banovina.hr/',
-        historyUrl: 'https://www.radio-banovina.hr/light/povijest.html',
-        currentSongUrl: 'https://pool.alter-media.hr:2020/json/stream/light',
-        currentSongDataFunc: banovinaSongDataFunc
-    },
-    { 
-        name: 'Banovina Turbo',
-        url: 'https://audio.radio-banovina.hr:7010/stream',
-        web: 'https://www.radio-banovina.hr/',
-        historyUrl: 'https://www.radio-banovina.hr/turbo/povijest.html',
-        currentSongUrl: 'https://pool.alter-media.hr:2020/json/stream/turbo',
-        currentSongDataFunc: banovinaSongDataFunc
-    },
-    { 
-        name: 'Đakovo',
-        url: 'https://ec2s.crolive.com.hr:8305/stream',
-        web: 'https://www.radio-djakovo.hr/',
-        frequencies: ['100,2', '89,6'],
-        currentSongUrl: 'dummy',
-        currentSongDataFunc: radioDjakovoSongDataFunc
-
-    },
-    { 
-        name: 'Slavonski',
-        url: 'https://ec2s.crolive.com.hr:8035/stream',
-        web: 'https://www.slavonskiradio.hr/',
-        frequencies: ['106,2', '100,6', '91,0', '89,7'],
-        currentSongUrl: 'dummy',
-        currentSongDataFunc: slavonskiSongDataFunc
-    },
-    { 
-        name: 'Antena Zagreb',
-        url: 'https://audio.social3.hr/listen/antena_aac/stream',
-        web: 'https://www.antenazagreb.hr/',
-        historyUrl: 'https://streaming.antenazagreb.hr/stream/player/player.html',
-        frequencies: ['89,7'],
-        currentSongUrl: 'https://streaming.antenazagreb.hr/stream/player/info/listen_antena_aac_.txt',
-        currentSongDataFunc: antenaSongDataFunc
-    },
-    { 
-        name: 'Otvoreni',
-        url: 'https://stream2.otvoreni.hr/otvoreni',
-        web: 'https://www.otvoreni.hr/',
-        frequencies: ['107,9', '107,5', '107,3', '105,6', '104,4', '104,3', '104,2', '103,3', '102,6', 
-                        '99,3', '97,7', '93,2', '97,3', '92,6', '92,5', '91,2', '91,1', '90,6', '89,6', '88,3', '87,6'],
-        currentSongUrl: 'wss://s-usc1f-nss-2501.firebaseio.com/.ws?v=5&ns=otvoreni-radio-player',
-        currentSongDataFunc: otvoreniSongDataFunc
-    }
+  {
+    name: "Tamburaški",
+    url: "https://listen.radioking.com/radio/552965/stream/612287",
+    web: "https://www.tamburaski.com/",
+    currentSongUrl: "https://api.radioking.io/widget/radio/tamburaskiradio/track/current",
+    currentSongDataFunc: tamburaskiSongDataFunc,
+  },
+  {
+    name: "DRS",
+    url: "https://eu2.fastcast4u.com/proxy/mic0?mp=/stream",
+    web: "https://drugacija.me/",
+    frequencies: ["104,8", "101,5", "97,1", "95,5", "93,0", "90,3"],
+    currentSongUrl: "https://eu2.fastcast4u.com/proxy/mic0/7.html",
+    currentSongDataFunc: DRSSongDataFunc,
+  },
+  {
+    name: "Radio Fortuna MK",
+    url: "https://radiofortuna.ipradio.mk/;",
+    web: "https://radiofortuna.com.mk/",
+    historyUrl: "https://radiofortuna.ipradio.mk/played.html",
+    currentSongDataFunc: fortunaSongDataFunc,
+  },
+  {
+    name: "Banovina Uživo",
+    url: "https://audio.radio-banovina.hr:9998/stream",
+    web: "https://www.radio-banovina.hr/",
+    frequencies: ["99,1", "96,8", "93,2"],
+    historyUrl: "https://www.radio-banovina.hr/uzivo/song_history.html",
+    currentSongUrl: "https://pool.alter-media.hr:2020/json/stream/banovina",
+    currentSongDataFunc: banovinaSongDataFunc,
+  },
+  {
+    name: "Banovina Light",
+    url: "https://audio.radio-banovina.hr:7008/stream",
+    web: "https://www.radio-banovina.hr/",
+    historyUrl: "https://www.radio-banovina.hr/light/povijest.html",
+    currentSongUrl: "https://pool.alter-media.hr:2020/json/stream/light",
+    currentSongDataFunc: banovinaSongDataFunc,
+  },
+  {
+    name: "Banovina Turbo",
+    url: "https://audio.radio-banovina.hr:7010/stream",
+    web: "https://www.radio-banovina.hr/",
+    historyUrl: "https://www.radio-banovina.hr/turbo/povijest.html",
+    currentSongUrl: "https://pool.alter-media.hr:2020/json/stream/turbo",
+    currentSongDataFunc: banovinaSongDataFunc,
+  },
+  {
+    name: "Đakovo",
+    url: "https://ec2s.crolive.com.hr:8305/stream",
+    web: "https://www.radio-djakovo.hr/",
+    frequencies: ["100,2", "89,6"],
+    currentSongDataFunc: radioDjakovoSongDataFunc,
+  },
+  {
+    name: "Slavonski",
+    url: "https://ec2s.crolive.com.hr:8035/stream",
+    web: "https://www.slavonskiradio.hr/",
+    frequencies: ["106,2", "100,6", "91,0", "89,7"],
+    currentSongDataFunc: slavonskiSongDataFunc,
+  },
+  {
+    name: "Antena Zagreb",
+    url: "https://audio.social3.hr/listen/antena_aac/stream",
+    web: "https://www.antenazagreb.hr/",
+    historyUrl: "https://streaming.antenazagreb.hr/stream/player/player.html",
+    frequencies: ["89,7"],
+    currentSongUrl: "https://streaming.antenazagreb.hr/stream/player/info/listen_antena_aac_.txt",
+    currentSongDataFunc: antenaSongDataFunc,
+  },
+  {
+    name: "Otvoreni",
+    url: "https://stream2.otvoreni.hr/otvoreni",
+    web: "https://www.otvoreni.hr/",
+    frequencies: [
+      "107,9",
+      "107,5",
+      "107,3",
+      "105,6",
+      "104,4",
+      "104,3",
+      "104,2",
+      "103,3",
+      "102,6",
+      "99,3",
+      "97,7",
+      "93,2",
+      "97,3",
+      "92,6",
+      "92,5",
+      "91,2",
+      "91,1",
+      "90,6",
+      "89,6",
+      "88,3",
+      "87,6",
+    ],
+    currentSongUrl: "wss://s-usc1f-nss-2501.firebaseio.com/.ws?v=5&ns=otvoreni-radio-player",
+    currentSongDataFunc: otvoreniSongDataFunc,
+  },
 ];
 
 export default streams;
